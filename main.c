af01c7933742b8721ea3db0c973d95a06f6f3b19
didas72 <diogocruzdiniz@gmail.com>
2023-12-12 18:27:57 +0000

    More organization and fixes

--- main.c
+++ main.c
@@ -16,8 +16,9 @@
 void process_args(int argc, char *argv[]);
 unsigned int launch_processes(DIR* dir);
 int child_main(struct dirent *dirent);
-int processFile(struct dirent *dirent);
-void handleFile(int input_no, int output_no);
+int process_file(struct dirent *dirent);
+void handle_file(int input_no, int output_no);
+int handle_command(enum Command cmd, int input_no, int output_no);
 
 unsigned int state_access_delay_ms = STATE_ACCESS_DELAY_MS;
 unsigned int max_proc = MAX_PROC, max_thread = MAX_THREADS;
@@ -133,13 +134,13 @@ unsigned int launch_processes(DIR* dir)
 
 int child_main(struct dirent *dirent)
 {
-  int verify = processFile(dirent);
+  int verify = process_file(dirent);
   if (verify == SUCESS)
     return EXIT_SUCCESS;
   return EXIT_FAILURE;
 }
 
-int processFile(struct dirent *dirent)
+int process_file(struct dirent *dirent)
 {
   // Could be done later
   char relativePath[1024]; // FIXME: Better size?
@@ -162,113 +163,135 @@ int processFile(struct dirent *dirent)
 
   strcpy(ext, ".out");
   int output_no = open(relativePath, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-  handleFile(input_no, output_no);
+  handle_file(input_no, output_no);
   close(input_no);
   close(output_no);
   return SUCESS;
 }
 
-void handleFile(int input_no, int output_no)
+void handle_file(int input_no, int output_no)
 {
   while (1)
   {
-    unsigned int event_id, delay;
-    size_t num_rows, num_columns, num_coords;
-    size_t xs[MAX_RESERVATION_SIZE], ys[MAX_RESERVATION_SIZE];
+    enum Command cmd = get_next(input_no);
 
-    switch (get_next(input_no))
+    if (cmd == CMD_BARRIER)
     {
-    case CMD_CREATE:
-      if (parse_create(input_no, &event_id, &num_rows, &num_columns) != 0)
-      {
-        fprintf(stderr, "Invalid command. See HELP for usage\n");
-        continue;
-      }
-
-      if (ems_create(event_id, num_rows, num_columns))
-      {
-        fprintf(stderr, "Failed to create event\n");
-      }
-
-      break;
-
-    case CMD_RESERVE:
-      num_coords = parse_reserve(input_no, MAX_RESERVATION_SIZE, &event_id, xs, ys);
-
-      if (num_coords == 0)
-      {
-        fprintf(stderr, "Invalid command. See HELP for usage\n");
-        continue;
-      }
-
-      if (ems_reserve(event_id, num_coords, xs, ys))
-      {
-        fprintf(stderr, "Failed to reserve seats\n");
-      }
-
-      break;
-
-    case CMD_SHOW:
-      if (parse_show(input_no, &event_id) != 0)
-      {
-        fprintf(stderr, "Invalid command. See HELP for usage\n");
-        continue;
-      }
-
-      if (ems_show(event_id, output_no))
-      {
-        fprintf(stderr, "Failed to show event\n");
-      }
-
-      break;
-
-    case CMD_LIST_EVENTS:
-      if (ems_list_events(output_no))
-      {
-        fprintf(stderr, "Failed to list events\n");
-      }
-
-      break;
-
-    case CMD_WAIT:
-      if (parse_wait(input_no, &delay, NULL) == -1)
-      { // thread_id is not implemented
-        fprintf(stderr, "Invalid command. See HELP for usage\n");
-        continue;
-      }
-
-      if (delay > 0)
-      {
-        printf("Waiting...\n");
-        ems_wait(delay);
-      }
-
-      break;
-
-    case CMD_INVALID:
-      fprintf(stderr, "Invalid command. See HELP for usage\n");
-      break;
-
-    case CMD_HELP:
-      printf(
-          "Available commands:\n"
-          "  CREATE <event_id> <num_rows> <num_columns>\n"
-          "  RESERVE <event_id> [(<x1>,<y1>) (<x2>,<y2>) ...]\n"
-          "  SHOW <event_id>\n"
-          "  LIST\n"
-          "  WAIT <delay_ms> [thread_id]\n" // thread_id is not implemented
-          "  BARRIER\n"                     // Not implemented
-          "  HELP\n");
-
-      break;
-
-    case CMD_BARRIER: // Not implemented
-    case CMD_EMPTY:
-      break;
-
-    case EOC:
-      // TODO: Write events to output file
-      return;
+      //Send exit signal to all threads
+    }
+    else if (cmd == CMD_WAIT)
+    {
+      //If global wait, must send some signal to all threads
+      //If not, can send to corresponding thread
+    }
+    else
+    {
+      if (handle_command(cmd, input_no, output_no))
+        break;
     }
   }
+}
+
+int handle_command(enum Command cmd, int input_no, int output_no)
+{
+  unsigned int event_id, delay;
+  size_t num_rows, num_columns, num_coords;
+  size_t xs[MAX_RESERVATION_SIZE], ys[MAX_RESERVATION_SIZE];
+
+  switch (cmd)
+  {
+  case CMD_CREATE:
+    if (parse_create(input_no, &event_id, &num_rows, &num_columns) != 0)
+    {
+      fprintf(stderr, "Invalid command. See HELP for usage\n");
+      break;
+    }
+
+    if (ems_create(event_id, num_rows, num_columns))
+    {
+      fprintf(stderr, "Failed to create event\n");
+    }
+
+    break;
+
+  case CMD_RESERVE:
+    num_coords = parse_reserve(input_no, MAX_RESERVATION_SIZE, &event_id, xs, ys);
+
+    if (num_coords == 0)
+    {
+      fprintf(stderr, "Invalid command. See HELP for usage\n");
+      break;
+    }
+
+    if (ems_reserve(event_id, num_coords, xs, ys))
+    {
+      fprintf(stderr, "Failed to reserve seats\n");
+    }
+
+    break;
+
+  case CMD_SHOW:
+    if (parse_show(input_no, &event_id) != 0)
+    {
+      fprintf(stderr, "Invalid command. See HELP for usage\n");
+      break;
+    }
+
+    if (ems_show(event_id, output_no))
+    {
+      fprintf(stderr, "Failed to show event\n");
+    }
+
+    break;
+
+  case CMD_LIST_EVENTS:
+    if (ems_list_events(output_no))
+    {
+      fprintf(stderr, "Failed to list events\n");
+    }
+
+    break;
+
+  case CMD_WAIT:
+    if (parse_wait(input_no, &delay, NULL) == -1)
+    { // thread_id is not implemented
+      fprintf(stderr, "Invalid command. See HELP for usage\n");
+      break;
+    }
+
+    if (delay > 0)
+    {
+      printf("Waiting...\n");
+      ems_wait(delay);
+    }
+
+    break;
+
+  case CMD_INVALID:
+    fprintf(stderr, "Invalid command. See HELP for usage\n");
+    break;
+
+  case CMD_HELP:
+    printf(
+        "Available commands:\n"
+        "  CREATE <event_id> <num_rows> <num_columns>\n"
+        "  RESERVE <event_id> [(<x1>,<y1>) (<x2>,<y2>) ...]\n"
+        "  SHOW <event_id>\n"
+        "  LIST\n"
+        "  WAIT <delay_ms> [thread_id]\n" // thread_id is not implemented
+        "  BARRIER\n"                     // Handled separately
+        "  HELP\n");
+
+    break;
+
+  case CMD_BARRIER: // Handled separately
+  case CMD_EMPTY:
+    break;
+
+  case EOC:
+    // TODO: Write events to output file
+    return 1;
+  }
+
+  return 0;
 }
